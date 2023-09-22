e3cafd5d78f5141db29a510bd066b87506712af0
crus17 <christopher.crus17@gmail.com>
2023-09-22 23:16:10 +0100

    .htaccess file updated

---
+++ storage/framework/views/0547fc97216fc3356c433a3ecd3a3ccaf4011920.php
@@ -0,0 +1,7 @@
+<div class="mt-2 mb-1">
+    <h1 class="title1">
+        <?php echo e($slot); ?>
+
+    </h1>
+</div>
+<?php /**PATH /Users/7thpriest/Codes/PHP Projects/Rough/mi360/resources/views/components/page-title.blade.php ENDPATH**/ ?>
