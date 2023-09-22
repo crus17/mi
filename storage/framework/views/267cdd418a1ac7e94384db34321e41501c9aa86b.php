fc45190b5263dbbc2ec45dc87af67cfacefb610b
crus17 <christopher.crus17@gmail.com>
2023-09-22 17:43:29 +0100

    Initial release

--- storage/framework/views/267cdd418a1ac7e94384db34321e41501c9aa86b.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /home/u548031247/domains/rveasy.net/public_html/crypto/resources/views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
